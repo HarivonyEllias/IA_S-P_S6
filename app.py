7384c34825fdcc7845a486a281062303c1a6d551
Rakotomavo Manoa Harivony Ellias <114306980+HarivonyEllias@users.noreply.github.com>
2025-07-17 21:06:43 +0300

    Deploy to Render

--- app.py
+++ app.py
@@ -200,5 +200,6 @@ def test():
     
     return render_template('result_test.html', efficiency=efficiency)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
+
